7538701bd3b2035b9d53f99c1cddada76eaf6363
Shane <shanedoyle21@hotmail.com>
2018-11-26 22:37:52 +0000

    Commit 23
    Player improvements and snowbomb adjustments.

--- Assets/Scripts/Snowbomb/RotateBall.cs
+++ Assets/Scripts/Snowbomb/RotateBall.cs
@@ -5,20 +5,4 @@ using UnityEngine;
 public class RotateBall : MonoBehaviour
 {
 
-    public GameObject obj = GameObject.Find("Snowbomb");
-
-    // Use this for initialization
-    void Start ()
-    {
-		
-	}
-
-
-    // Update is called once per frame
-    void Update ()
-    {
-        transform.Rotate(0, 0, 3);
-        obj.transform.Rotate(0, 0, 3);
-    }
-
 }
